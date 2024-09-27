3927bf137c369b7ea0fc2d0ef7524c3050f19532
KajalMakhija1810 <kajolmakhija1810@gmail.com>
2024-09-27 08:23:54 +0530

    Add Day3 Java practice files

---
+++ P1.java
@@ -0,0 +1,18 @@
+package Day3;
+
+public class P1 
+{
+	public static void main(String args[])
+	{
+		
+		int num = Integer.parseInt(args[0]);
+		
+		if(num%2 ==0)
+		System.out.println("Even ");
+		
+		else {
+			System.out.println("Odd ");
+		}
+	}
+
+}
